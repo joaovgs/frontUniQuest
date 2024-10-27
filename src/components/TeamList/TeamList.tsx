c227b3ff41be86e657f2fb1a7be70726e8542d8e
iNattan <natanmart09@gmail.com>
2024-10-27 19:13:13 -0300

    Correção na tela de loading

--- src/components/TeamList/TeamList.tsx
+++ src/components/TeamList/TeamList.tsx
@@ -17,6 +17,7 @@ const TeamList: React.FC = () => {
   const [selectedCompetitionId, setSelectedCompetitionId] = useState<number>(Number(competitionId));
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTeam, setSelectedTeam] = useState<Team | null>(null);
+  const [loadingCompetitions, setLoadingCompetitions] = useState(true);
 
   const mapStatusToText = (status: number | null | undefined): string => {
     switch (status) {
@@ -45,6 +46,7 @@ const TeamList: React.FC = () => {
 
   useEffect(() => {
     const fetchCompetitions = async () => {
+      setLoadingCompetitions(true);
       try {
         const { competitions } = await CompetitionService.getCompetitions();
         setCompetitions(competitions);
@@ -53,6 +55,8 @@ const TeamList: React.FC = () => {
         }
       } catch (error) {
         console.error('Erro ao buscar gincanas:', error);
+      } finally {
+        setLoadingCompetitions(false);
       }
     };
 
@@ -95,55 +99,63 @@ const TeamList: React.FC = () => {
     <div className="team-overview-container">
       <h1>Inscrições das Equipes</h1>
 
-      <div className="filter-container">
-        <label htmlFor="competitionIdSelect">Selecionar Gincana:</label>
-        <select
-          id="competitionIdSelect"
-          value={selectedCompetitionId}
-          onChange={handleCompetitionIdChange}
-          className="competition-id-select"
-        >
-          {competitions.map((competition) => (
-            <option key={competition.id} value={competition.id}>
-              {competition.title}
-            </option>
-          ))}
-        </select>
-
-        <label htmlFor="statusFilter">Filtrar por status:</label>
-        <select
-          id="statusFilter"
-          value={filterStatus}
-          onChange={handleFilterChange}
-          className="status-filter"
-        >
-          <option value="Todos">Todos</option>
-          <option value="Pendente">Pendente</option>
-          <option value="Aprovada">Aprovada</option>
-          <option value="Rejeitada">Rejeitada</option>
-        </select>
-      </div>
-
-      {loadingTeams ? (
+      {loadingCompetitions ? (
         <div className="spinner-container">
           <Spinner />
         </div>
       ) : (
-        <div className="teams-grid">
-          {filteredTeams.length === 0 ? (
-            <p>Nenhuma equipe encontrada.</p>
+        <>
+          <div className="filter-container">
+            <label htmlFor="competitionIdSelect">Selecionar Gincana:</label>
+            <select
+              id="competitionIdSelect"
+              value={selectedCompetitionId}
+              onChange={handleCompetitionIdChange}
+              className="competition-id-select"
+            >
+              {competitions.map((competition) => (
+                <option key={competition.id} value={competition.id}>
+                  {competition.title}
+                </option>
+              ))}
+            </select>
+
+            <label htmlFor="statusFilter">Filtrar por status:</label>
+            <select
+              id="statusFilter"
+              value={filterStatus}
+              onChange={handleFilterChange}
+              className="status-filter"
+            >
+              <option value="Todos">Todos</option>
+              <option value="Pendente">Pendente</option>
+              <option value="Aprovada">Aprovada</option>
+              <option value="Rejeitada">Rejeitada</option>
+            </select>
+          </div>
+
+          {loadingTeams ? (
+            <div className="spinner-container">
+              <Spinner />
+            </div>
           ) : (
-            filteredTeams.map((team, index) => (
-              <div key={index} className="team-card" onClick={() => openModal(team)}>
-                <div className={`team-status ${mapStatusToText(team.status).toLowerCase()}`}>
-                  {mapStatusToText(team.status)}
-                </div>
-                <h3>{team.name}</h3>
-                <p>{team.members_count}/{team.max_participant}</p>
-              </div>
-            ))
+            <div className="teams-grid">
+              {filteredTeams.length === 0 ? (
+                <p>Nenhuma equipe encontrada.</p>
+              ) : (
+                filteredTeams.map((team, index) => (
+                  <div key={index} className="team-card" onClick={() => openModal(team)}>
+                    <div className={`team-status ${mapStatusToText(team.status).toLowerCase()}`}>
+                      {mapStatusToText(team.status)}
+                    </div>
+                    <h3>{team.name}</h3>
+                    <p>{team.members_count}/{team.max_participant}</p>
+                  </div>
+                ))
+              )}
+            </div>
           )}
-        </div>
+        </>
       )}
 
       {isModalOpen && selectedTeam && (
